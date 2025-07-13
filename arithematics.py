19ecedc1f84971f68ee553ee826e43ec4fa3c0d0
Shyamkumar61 <shyam6132@gmail.com>
2025-07-13 15:19:38 +0530

    div func added

--- arithematics.py
+++ arithematics.py
@@ -10,3 +10,10 @@ def mult(*args):
     for arg in args:
         value *= arg
     return value
+
+def div(*args):
+    value = 0
+    if len(args) >= 2:
+        for arg in range(0, len(args) - 1):
+            value = args[arg]/args[arg + 1]
+    return value
