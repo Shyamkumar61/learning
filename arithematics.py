8fa2fe008e540a85f120303202d75370377bd330
Shyamkumar61 <shyam6132@gmail.com>
2025-07-13 15:45:52 +0530

    main file version changed to 3

--- arithematics.py
+++ arithematics.py
@@ -11,9 +11,17 @@ def mult(*args):
         value *= arg
     return value
 
+
 def div(*args):
     value = 0
     if len(args) >= 2:
         for arg in range(0, len(args) - 1):
             value = args[arg]/args[arg + 1]
+    return value
+
+
+def sum(*args):
+    value = 0
+    for arg in range(0, len(args) - 1):
+        value -= args[arg]
     return value
